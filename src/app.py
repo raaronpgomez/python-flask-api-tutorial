8c5339d87918101aa659c22565e4832bed784f77
raaronpgomez <raaronpgomez@gmail.com>
2025-08-26 01:30:45 +0000

    flask api tutorial

---
+++ src/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+todos = [
+    { 
+        "label": "My first task", 
+        "done": False 
+    }
+]
+
+@app.route('/todos', methods=['GET'])
+def get_todos():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json 
+    print("Incoming request with the following body:", request_body)
+    
+    todos.append(request_body)
+    
+    return jsonify(todos), 200
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete:", position)
+    
+    if position < 0 or position >= len(todos):
+        return jsonify({"error": "Position out of range"}), 200
+    
+    todos.pop(position)
+    
+    return jsonify(todos), 200
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
